1e43787aa8f52028f441d6f52dd751dd08a5571a
sysfs <sysfs@users.noreply.github.com>
2026-09-24 21:19:48 +0200

    chore(tokenizer): add type hints

--- slm-from-scratch.py
+++ slm-from-scratch.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 # downloaded from https://www.dbnl.org/nieuws/text.php?id=coup002elin01
-text = open("dbnl.coup002elin01.txt").read()
+with open("dbnl.coup002elin01.txt", encoding="utf-8") as file:
+    text = file.read()
+
 chars = sorted(set(text))
 vocab_size = len(chars)
 
@@ -9,12 +11,12 @@ s_to_i = {c: i for i, c in enumerate(chars)}
 i_to_s = {i: c for c, i in s_to_i.items()}
 
 
-def encode(s):
+def encode(s: str) -> list[int]:
     return [s_to_i[c] for c in s]
 
 
-def decode(ids):
-    return "".join([i_to_s[i] for i in ids])
+def decode(ids: list[int]) -> str:
+    return "".join(i_to_s[i] for i in ids)
 
 
 if __name__ == "__main__":
